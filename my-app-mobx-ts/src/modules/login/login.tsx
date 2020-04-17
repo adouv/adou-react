a043454d2c6b05118b02e02cb36b33b262735dcc
kaihang.fu <kaihang.fu@oasismedical.cn>
2020-04-17 20:37:44 +0800

    d

---
+++ my-app-mobx-ts/src/modules/login/login.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './login.scss';
+/**
+ * 登录页面
+ */
+export default class LoginPage extends Component {
+    public render() {
+        return (
+            <div>我是登录页面</div>
+        );
+    }
+}
