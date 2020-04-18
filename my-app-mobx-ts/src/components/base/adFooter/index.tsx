6ae9f351233da152de56372d197da523aba8bb0d
kaihang.fu <kaihang.fu@oasismedical.cn>
2020-04-18 17:59:51 +0800

    d

---
+++ my-app-mobx-ts/src/components/base/adFooter/index.tsx
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+export default class AdFooterBaseComponent extends Component
+{
+    constructor(props:any){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div></div>
+        )
+    }
+}
